0b1629d347f79b405b5205e0ed230a5ac2f7bfe2
arzummammadova <arzuam-af106@code.edu.az>
2025-07-01 09:37:53 +0400

    added footer navbar

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,6 +13,7 @@ const Footer = () => {
               <li>Link 1</li>
               <li>Link 2</li>
               <li>Link 3</li>
+              <li>Link 4</li>
             </ul>
           </div>
           {/* Section 2 */}
